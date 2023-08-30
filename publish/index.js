321ba5718764fe31f3d5227da3c07383c4d6d1f3
suigang <suigang@css.com.cn>
2023-08-30 10:01:04 +0800

    feat: 去掉console

--- publish/index.js
+++ publish/index.js
@@ -3,7 +3,6 @@ import { pkgPath } from './utils/paths.js';
 import pkg from 'gulp';
 const { series } = pkg;
 export const publishComponent = async () => {
-  // console.log(`${pkgPath}/suiCli`,'---pa')
   run('release-it', `${pkgPath}/suiCli`);
 };
 export default series(async () => publishComponent());
